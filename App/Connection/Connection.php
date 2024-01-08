1fb4b4195cf825fc98d65a67f8776d9279a2de19
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-01-08 10:14:25 +0100

    connection to database

---
+++ App/Connection/Connection.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Connection;
+use PDO;
+use Dotenv\Dotenv;
+require_once __DIR__.'/../../vendor/autoload.php';
+$dotenv = Dotenv::CreateImmutable(__DIR__.'/../../');
+$dotenv->load();
+
+class Connection {
+    private static $instance;
+    private $connection;
+    public static $count = 0;
+
+    private function __construct(){
+        $servername = $_ENV['DB_HOST'];
+        $username = $_ENV['DB_NAME'];
+        $password = $_ENV['DB_PASSWORD'];
+        $dbname = $_ENV['DB_NAME'];
+        $this->connection = new PDO("mysql:host= $servername;dbname=$dbname ;$username , $password");
+
+        if(!$this->connection){
+            die("connection failed".mysqli_connect_error());
+        }else{
+            echo"connected successfully";
+        }
+    }
+    public static function getInstance(){
+        if(!isset(self::$instance)){
+            self::$instance = new Connection();
+        }
+        return self::$instance;
+    }
+
+    public function getConnect(){
+        return $this->connection;
+    }
+}
